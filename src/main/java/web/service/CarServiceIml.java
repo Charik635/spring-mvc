dab946f5a69a6f00e4d8f5a81641ca7f5241d167
Charik635 <cuber1319@gmail.com>
2021-07-16 18:08:39 +0300

    Bug Fix

--- src/main/java/web/service/CarServiceIml.java
+++ src/main/java/web/service/CarServiceIml.java
@@ -10,21 +10,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class CarServiceIml implements CarService{
+public class CarServiceIml implements CarService {
     private List<Car> carList;
+
     {
         carList = new ArrayList<>();
-        carList.add(new Car("Audi",1));
-        carList.add(new Car("BMV",2));
-        carList.add(new Car("Lada",3));
-        carList.add(new Car("Gazel",4));
-        carList.add(new Car("Kio",5));
+        carList.add(new Car("Audi", 1));
+        carList.add(new Car("BMV", 2));
+        carList.add(new Car("Lada", 3));
+        carList.add(new Car("Gazel", 4));
+        carList.add(new Car("Kio", 5));
     }
 
     @Override
     public List<Car> show(Integer id) {
-     Integer count = id == null ? 5 : id;
-        return  carList.stream().filter(carlist -> carlist.getId() <=count).collect(Collectors.toList());
+        Integer count = id == null ? 5 : id;
+        return carList.stream().filter(carlist -> carlist.getId() <= count).collect(Collectors.toList());
     }
 
 
