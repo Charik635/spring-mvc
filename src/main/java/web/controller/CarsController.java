668676c073d58d51fa90bd89fbb8d8c25c0ccde2
Charik635 <cuber1319@gmail.com>
2021-07-16 17:40:51 +0300

    Ready

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,29 +1,23 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarService;
 import web.service.CarServiceIml;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class CarsController {
 
-    private CarService carService;
+    private CarServiceIml carServiceIml;
 
-    public CarsController(CarService carService) {
-        this.carService = carService;
+    public CarsController(CarServiceIml carServiceIml) {
+        this.carServiceIml = carServiceIml;
     }
 
     @GetMapping("/cars")
     public String getAllCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        count = count == null ? 5 : count;
-            model.addAttribute("cars", carService.show(count));
+            model.addAttribute("cars", carServiceIml.show(count));
         return "cars";
     }
 
--- src/main/java/web/service/CarServiceIml.java
+++ src/main/java/web/service/CarServiceIml.java
@@ -1,10 +1,31 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public interface CarServiceIml {
+@Service
+public class CarServiceIml implements CarService{
+    private List<Car> carList;
+    {
+        carList = new ArrayList<>();
+        carList.add(new Car("Audi",1));
+        carList.add(new Car("BMV",2));
+        carList.add(new Car("Lada",3));
+        carList.add(new Car("Gazel",4));
+        carList.add(new Car("Kio",5));
+    }
 
-    List<Car> show(int id);
+    @Override
+    public List<Car> show(Integer id) {
+     Integer count = id == null ? 5 : id;
+        return  carList.stream().filter(carlist -> carlist.getId() <=count).collect(Collectors.toList());
+    }
+
+
 }
